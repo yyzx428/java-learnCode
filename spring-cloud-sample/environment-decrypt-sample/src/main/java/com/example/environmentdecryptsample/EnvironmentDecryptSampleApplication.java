ba2bf88063f2deeee7328d32e95c0fb6b5db3d4a
yyzx <zhkun.yin@gmail.com>
2019-03-21 22:36:09 +0800

    spring-cloud-config 配置增加加解密设置

--- spring-cloud-sample/environment-decrypt-sample/src/main/java/com/example/environmentdecryptsample/EnvironmentDecryptSampleApplication.java
+++ spring-cloud-sample/environment-decrypt-sample/src/main/java/com/example/environmentdecryptsample/EnvironmentDecryptSampleApplication.java
@@ -1,13 +1,28 @@
 package com.example.environmentdecryptsample;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnvironmentDecryptSampleApplication {
+public class EnvironmentDecryptSampleApplication implements ApplicationRunner {
+
+    @Value("${foo}")
+    private String foo;
+
+    @Value("${foo1}")
+    private String foo1;
 
     public static void main(String[] args) {
         SpringApplication.run(EnvironmentDecryptSampleApplication.class, args);
     }
 
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(foo);
+        System.out.println(foo1);
+    }
 }
