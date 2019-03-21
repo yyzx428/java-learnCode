16c3a9e5577b2f5ac4b5af8cd9f7a5de74d69ab9
yyzx <zhkun.yin@gmail.com>
2019-03-21 16:27:21 +0800

    spring-cloud 初始化

---
+++ spring-cloud-sample/environment-decrypt-sample/src/main/java/com/example/environmentdecryptsample/EnvironmentDecryptSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.environmentdecryptsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentDecryptSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvironmentDecryptSampleApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-sample/environment-decrypt-sample/src/test/java/com/example/environmentdecryptsample/EnvironmentDecryptSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.environmentdecryptsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnvironmentDecryptSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
