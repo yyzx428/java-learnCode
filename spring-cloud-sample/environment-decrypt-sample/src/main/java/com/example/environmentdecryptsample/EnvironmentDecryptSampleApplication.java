dc3dfffcd6575a377df3e854034925037c6b440c
yyzx <zhkun.yin@gmail.com>
2019-03-23 11:33:53 +0800

    完成注册中心和配置中心的连接

--- spring-cloud-sample/environment-decrypt-sample/src/main/java/com/example/environmentdecryptsample/EnvironmentDecryptSampleApplication.java
+++ spring-cloud-sample/environment-decrypt-sample/src/main/java/com/example/environmentdecryptsample/EnvironmentDecryptSampleApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EnvironmentDecryptSampleApplication implements ApplicationRunner {
 
     @Value("${foo}")
